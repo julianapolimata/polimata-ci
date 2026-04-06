eafb52bbc851b01746eecee6773aba55cc1575d7
Juliana <juliana@polimatagrc.com.br>
2026-04-06 20:23:17 -0300

    fix: remover registerServiceWorker

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
-import { registerServiceWorker } from './lib/service-worker-register'
 import App from './App'
 import './index.css'
-
-// Registrar service worker pra cache busting
-registerServiceWorker()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
